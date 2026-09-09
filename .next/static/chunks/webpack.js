147329a748cb7fbc1254b11ba10a20ef5bacacc8
Mugilan <mugilanm23112005@gmail.com>
2026-09-09 17:56:42 +0530

    fix deploy - npm run building

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0c8a4e35489c6994"; }
+/******/ 		__webpack_require__.h = function() { return "dc5d6f06a6cf30b2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
